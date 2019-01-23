031a84f63d14be34e5e656e872b9cb39e1720caa
Kevin Wong <wertykevin91@gmail.com>
2019-01-24 02:24:15 +0800

    changed deployment to suit new contract format

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -15,6 +15,18 @@ module.exports = function(deployer, network, accounts){
     deployer.deploy(safeMath);
     deployer.link(safeMath, businessLogicContract);
 
+    // CHANGE THIS PARAMETER DURING DEPLOYMENT
+    let contractOwner = accounts[1];
+    let contractAdmin = accounts[0];
+
+    if(network == "ropsten"){
+        contractOwner = "0xE4A8E9a0fC38293191302fc5F373f25EAd4c5E04";
+        contractAdmin = "0x15c0fA3a553F6DC1AF3F950565e5f4403aE62f72";
+    }
+
+    //console.log(network);
+    //var acc = null;
+
     let helperInstance = null;
     let transactionCount = 0;
 
@@ -24,10 +36,15 @@ module.exports = function(deployer, network, accounts){
     let businessContractInstance = null;
     let dataContractInstance = null;
     
+    // web3.eth.getAccounts().then(function(x){
+    //     acc = x;
+    //     console.log(acc);
+    //     return deployer.deploy(returnKeccakHash);
+    // })
     deployer.deploy(returnKeccakHash)
     .then(function(instance){
         helperInstance = instance;
-        return web3.eth.getTransactionCount(accounts[1]);
+        return web3.eth.getTransactionCount(contractOwner);
     })
     .then(function(n){
         transactionCount = n;
@@ -38,22 +55,25 @@ module.exports = function(deployer, network, accounts){
         if(hex == 0x00)
             hex = 0x80;
 
-        return helperInstance.keccakHash.call(accounts[1], hex);
+        return helperInstance.keccakHash.call(contractOwner, hex);
     }).then(function(n){
         contractAdd1 = n;
-        return helperInstance.keccakHash.call(accounts[1], web3.utils.toHex(transactionCount + 1));
+        return helperInstance.keccakHash.call(contractOwner, web3.utils.toHex(transactionCount + 1));
     }).then(function(n){
         contractAdd2 = n;
 
-        return deployer.deploy(dataLayerContract, contractAdd2, {from: accounts[1]});
+        return deployer.deploy(dataLayerContract, contractAdd2, {from: contractOwner});
     }).then(function(instance){
         dataContractInstance = instance;
-        //console.log(dataContractInstance.address + " : " + contractAdd1);
+        console.log(dataContractInstance.address + " : " + contractAdd1);
 
-        return deployer.deploy(businessLogicContract, tokenConfig.tokenName, tokenConfig.tokenDecimals, tokenConfig.tokenSymbol, tokenConfig.tokenTotalSupply, accounts[0], contractAdd1, {from: accounts[1]});
+        return deployer.deploy(businessLogicContract, tokenConfig.tokenName, tokenConfig.tokenDecimals, tokenConfig.tokenSymbol, contractAdmin, {from: contractOwner});
     }).then(function(instance){
         businessContractInstance = instance;
 
-        //console.log(businessContractInstance.address + " : " + contractAdd2);
+        console.log(businessContractInstance.address + " : " + contractAdd2);
+        return businessContractInstance.firstTimeInit(tokenConfig.tokenTotalSupply, contractAdd1, {from: contractOwner});
+    }).then(function(){
+        console.log("Success!");
     });
 }
